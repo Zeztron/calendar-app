c42461181aef46291d0f65d7e4847f5e91eea74d
Zeztron <pateltharsh@gmail.com>
2019-01-10 10:21:58 -0500

    translated month and date

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ function updateCurrentDates() {
 }
 
 function addOrdinalIndicator(date) {
-    switch (date) {
+    switch(date) {
         case 1:
         case 21:
         case 31: return date + "<sup>st</sup>";
@@ -54,11 +54,51 @@ function addOrdinalIndicator(date) {
 }
 
 function translateToWeekdayName(day) {
-
+    switch (day) {
+      case 0:
+        return "Sunday";
+      case 1:
+        return "Monday";
+      case 2:
+        return "Tuesday";
+      case 3:
+        return "Wednesday";
+      case 4:
+        return "Thursday";
+      case 5:
+        return "Friday";
+      case 6:
+        return "Saturday";
+    }
 }
 
 function translateToMonthName(month) {
-
+    switch (month) {
+      case 0:
+        return "January";
+      case 1:
+        return "February";
+      case 2:
+        return "March";
+      case 3:
+        return "April";
+      case 4:
+        return "May";
+      case 5:
+        return "June";
+      case 6:
+        return "July";
+      case 7:
+        return "August";
+      case 8:
+        return "September";
+      case 9:
+        return "October";
+      case 10:
+        return "November";
+      case 11:
+        return "December";
+    }
 }
 
 updateCurrentDates();
